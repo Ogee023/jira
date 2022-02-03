dcb5cf934ab3e4b587c3ddd97d6f84c639234cc2
十一 <7660068+ogee023@user.noreply.gitee.com>
2022-02-03 19:44:36 +0800

    style: 优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import qs from "qs"
 import { cleanObject, useDebounce, useMount } from "../../utils"
 import { useHttp } from "utils/http"
+import styled from "@emotion/styled"
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -26,8 +27,13 @@ export const ProjectListScreen = () => {
     client('users').then(setUsers)
   })
 
-  return <div>
+  return <Container>
+    <h1>项目列表</h1>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List users={users} list={list} />
-  </div>
+  </Container>
 }
+
+const Container = styled.div`
+  padding: 3.2rem;
+`
