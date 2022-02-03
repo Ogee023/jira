5d1560749ca2150a5cf1c24130c29ec7d833421f
十一 <7660068+ogee023@user.noreply.gitee.com>
2022-02-03 20:43:34 +0800

    style: 完成样式

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,12 +1,9 @@
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useEffect, useState } from "react"
-import qs from "qs"
 import { cleanObject, useDebounce, useMount } from "../../utils"
 import { useHttp } from "utils/http"
 import styled from "@emotion/styled"
-
-const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([])
@@ -20,7 +17,8 @@ export const ProjectListScreen = () => {
   const client = useHttp()
 
   useEffect(() => {
-    client('projects', {data: cleanObject(debouncedParam)}).then(setList)
+    client('projects', { data: cleanObject(debouncedParam) }).then(setList)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedParam])
 
   useMount(() => {
