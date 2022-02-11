ec0372f2160ef549574c98b02ff2a6ab8e532ee2
十一 <7660068+ogee023@user.noreply.gitee.com>
2022-02-11 20:47:30 +0800

    chore: 用useCallback解决无限渲染问题

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { useMountedRef } from "utils"
 
 interface State<D> {
@@ -30,20 +30,20 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
 
   })
 
-  const setData = (data: D) => setState({
+  const setData = useCallback((data: D) => setState({
     data,
     stat: 'success',
     error: null
-  })
+  }), [])
 
-  const setError = (error: Error) => setState({
+  const setError = useCallback((error: Error) => setState({
     error,
     stat: 'error',
     data: null
-  })
+  }), [])
 
   // run用来出发异步请求
-  const run = (promise: Promise<D>, runConfig?: {retry: () => Promise<D>}) => {
+  const run = useCallback((promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
     if (!promise || !promise.then) {
       throw new Error('请传入Promise类型数据')
     }
@@ -52,20 +52,20 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
         run(runConfig?.retry(), runConfig)
       }
     })
-    setState({ ...state, stat: 'loading' })
+    setState(prevState => ({ ...prevState, stat: 'loading' }))
     return promise
       .then(data => {
         if (mountedRef.current)
-        setData(data)
+          setData(data)
         return data
       })
       .catch(error => {
         // catch会消化异常，如果不主动抛出，外面是接收不到异常的
         setError(error)
-        if(config.throwOnError) return Promise.reject(error)
+        if (config.throwOnError) return Promise.reject(error)
         return error
-    })
-  }
+      })
+  }, [config.throwOnError, mountedRef, setData, setError])
 
   return {
     isIdle: state.stat === 'idle',
