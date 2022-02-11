625ded4f638134278056c44a7ade00171c5f83a7
十一 <7660068+ogee023@user.noreply.gitee.com>
2022-02-11 19:40:29 +0800

    fix: 阻止在已经卸载组件上赋值

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useMountedRef } from "utils"
 
 interface State<D> {
   error: Error | null;
@@ -22,6 +23,8 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
     ...defaultInitialState,
     ...initialState
   })
+
+  const mountedRef = useMountedRef()
 
   const [retry, setRetry] = useState(() => () => {
 
@@ -52,6 +55,7 @@ export const useAsync = <D>(initialState?: State<D>, initialConfig?: typeof defa
     setState({ ...state, stat: 'loading' })
     return promise
       .then(data => {
+        if (mountedRef.current)
         setData(data)
         return data
       })
