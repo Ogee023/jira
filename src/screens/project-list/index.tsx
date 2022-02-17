94e6bae7712a25b0ad293938095c070994a9972a
ogeeou <ogeeou@trussan.com>
2022-02-17 13:29:00 +0800

    styles: 修复项目列表的样式

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,12 +1,11 @@
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useDebounce, useDocumentTitle } from "utils"
-import styled from "@emotion/styled"
 import { useProjects } from "utils/project"
 import { useUsers } from "utils/user"
 import { Row } from "antd"
 import { useProjectModal, useProjectsSearchParams } from "./util"
-import { ButtonNoPadding, ErrorBox } from "components/lib"
+import { ButtonNoPadding, ErrorBox, ScreenContainer } from "components/lib"
 
 // 基本类型和组件状态可以放到依赖里，非组件状态的对象绝不可以放到依赖里
 
@@ -19,7 +18,7 @@ export const ProjectListScreen = () => {
   const { isLoading, error, data: list } = useProjects(useDebounce(param, 200))
   const { data: users } = useUsers()
 
-  return <Container>
+  return <ScreenContainer>
     <Row justify="space-between" align="middle">
       <h1>项目列表</h1>
       <ButtonNoPadding
@@ -32,11 +31,7 @@ export const ProjectListScreen = () => {
     <SearchPanel users={users || []} param={param} setParam={setParam} />
     <ErrorBox error={error} />
     <List loading={isLoading} users={users || []} dataSource={list || []} />
-  </Container>
+  </ScreenContainer>
 }
 
 ProjectListScreen.whyDidYouRender = false
-
-const Container = styled.div`
-  padding: 3.2rem;
-`
